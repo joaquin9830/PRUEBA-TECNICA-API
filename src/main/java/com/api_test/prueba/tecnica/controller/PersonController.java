3d938558329ee0f7b6c1347395a9d16f8666ddcc
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 10:52:25 -0300

    Añadiendo las anotaciones de controller

--- src/main/java/com/api_test/prueba/tecnica/controller/PersonController.java
+++ src/main/java/com/api_test/prueba/tecnica/controller/PersonController.java
@@ -1,5 +1,9 @@
 package com.api_test.prueba.tecnica.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/persons")
 public class PersonController {
 }
