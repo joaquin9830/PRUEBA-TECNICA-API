806cf3dd030d75c10ae282b530ca340c2a2bdf08
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 11:18:10 -0300

    Creando los getter | setter de la clase Movie

--- src/main/java/com/api_test/prueba/tecnica/modelo/Movie.java
+++ src/main/java/com/api_test/prueba/tecnica/modelo/Movie.java
@@ -18,4 +18,30 @@ public class Movie {
     //Atributos de la clase
     private String title;
     private String genre;
+
+    //Getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
