5c268d322b2705bce224c14187a4972a079d0b24
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 10:42:32 -0300

    Agregando anotaciones y atributos de la clase persona

--- src/main/java/com/api_test/prueba/tecnica/modelo/Person.java
+++ src/main/java/com/api_test/prueba/tecnica/modelo/Person.java
@@ -1,10 +1,17 @@
 package com.api_test.prueba.tecnica.modelo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity
 public class Person {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
