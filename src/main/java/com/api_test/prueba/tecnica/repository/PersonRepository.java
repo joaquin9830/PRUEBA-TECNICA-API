e39232278d28e4eeec5255e4dc1a5ada69cd4ff8
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 10:50:55 -0300

    Repositorio de persona | importaciones listas

--- src/main/java/com/api_test/prueba/tecnica/repository/PersonRepository.java
+++ src/main/java/com/api_test/prueba/tecnica/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.api_test.prueba.tecnica.repository;
 import com.api_test.prueba.tecnica.modelo.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByFirstNameContaingIgnoreCase(String firstName);
 }
