b124e861bd97be6f2c0e085d65a9d843907170be
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 11:04:04 -0300

    Anotaciones de movie controller

--- src/main/java/com/api_test/prueba/tecnica/controller/MovieController.java
+++ src/main/java/com/api_test/prueba/tecnica/controller/MovieController.java
@@ -1,4 +1,9 @@
 package com.api_test.prueba.tecnica.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/persons/{personId}/movies")
 public class MovieController {
 }
