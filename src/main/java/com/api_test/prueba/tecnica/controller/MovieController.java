a303b228b5e91dfe79b1c4017871bd81e818e58b
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 11:05:02 -0300

    Métodos de la clase MovieControlle

--- src/main/java/com/api_test/prueba/tecnica/controller/MovieController.java
+++ src/main/java/com/api_test/prueba/tecnica/controller/MovieController.java
@@ -1,9 +1,31 @@
 package com.api_test.prueba.tecnica.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api_test.prueba.tecnica.modelo.Movie;
+import com.api_test.prueba.tecnica.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/persons/{personId}/movies")
 public class MovieController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Movie> getMovies(@PathVariable Long personId) {
+        return personService.getMoviesByPersonId(personId);
+    }
+
+    @PostMapping
+    public Movie addMovie(@PathVariable Long personId, @RequestBody Movie movie) {
+        return personService.addMovie(personId, movie);
+    }
+
+    @DeleteMapping("/{movieId}")
+    public void removeMovie(@PathVariable Long personId, @PathVariable Long movieId) {
+        personService.removeMovie(personId, movieId);
+    }
 }
