7509036763a1681fc5ff73c35ed04520385a334e
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 10:36:22 -0300

    Iniciando prueba tecnica

---
+++ src/test/java/com/api_test/prueba/tecnica/PruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_test.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
