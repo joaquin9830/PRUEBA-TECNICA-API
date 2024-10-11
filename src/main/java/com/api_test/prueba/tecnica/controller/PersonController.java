854db2790b197e7edaa9a9561faf964bced3719e
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 11:02:42 -0300

    Configuración de personcontroller

--- src/main/java/com/api_test/prueba/tecnica/controller/PersonController.java
+++ src/main/java/com/api_test/prueba/tecnica/controller/PersonController.java
@@ -1,12 +1,46 @@
 package com.api_test.prueba.tecnica.controller;
 
+import com.api_test.prueba.tecnica.modelo.Person;
+import com.api_test.prueba.tecnica.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/persons")
 public class PersonController {
 
+    @Autowired
+    private PersonService personService;
 
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable Long id) {
+        return personService.getPersonById(id);
+    }
+
+    @GetMapping("/search")
+    public List<Person> searchByName(@RequestParam("name") String name) {
+        return personService.getPersonsByName(name);
+    }
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable Long id, @RequestBody Person person) {
+        return personService.updatePerson(id, person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+    }
 }
